79cb2d3ec12cf30043cf41229887ebf2f283eb29
Zahra Huseynova <zahra.huseynova@gmail.com>
2021-09-15 14:19:40 +0400

    change to hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,49 @@
 import React from "react";
-import InputBar from "./InputBar";
+import TodoList from "./TodoList";
+import "./App.css"
+import { useState } from "react";
 
-class App extends React.Component {
-    
-    render () {
-        return (
-            <div><InputBar/></div>
-        )
+const App=({items})=>{
+    const [term , setTerm]= useState ([]);
+    const [addToList ,setAddToList] = useState("");
+
+    addTodo = (e) =>{
+        e.preventDefault();
+        const term=[... term]
+        term.push(addToList);
+
+        console.log(term);
+
+        setTerm({
+            addToList:"",
+            term,
+        });
     };
-      
+    
+    
+        return (
+            <div>
+                <div  className=" inputBar ui container">
+            <h1>ToDo App</h1>
+            <div className="ui right icon input">
+                    
+                <input 
+                    type="text" 
+                    placeholder="Add your task..." 
+                    value={addToList} 
+                    onChange={(e)=>{setAddToList(addToList(e.target.value)}
+                    />
+                <i className="plus squere icon"></i>
+                <div className="ui button" onClick={addTodo}></div>
+                    
+            </div>
+               
+                
+            </div>
+                <div><TodoList addList={term}/></div>
+            </div>
+        )
 };
-
-
 
 
 export default App;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,16 @@
 import React from "react";
+import "./Todo.css"
 
 
 const TodoList =(props)=> {
     
         return (
-            <div className="ui middle aligned divided list">
+            <div className="toDoList ui middle aligned divided list">
                 {
                 props.addList.map((task,index)=>{
                     return(
 
-                        <div key={index} className="item">
+                        <div key={index}   className="item">
                             <div className="right floated content">
                                 <div className="ui button"><i className="edit icon"></i></div>
                             </div>
